074a91365c56d00eecea8b3b6024d3e70e0191df
fadegor05 <fad.egor05@gmail.com>
2024-04-06 19:00:33 +0400

    added user registration on /start

--- app/handlers/start/start.py
+++ app/handlers/start/start.py
@@ -2,10 +2,14 @@ from aiogram.types import Message
 from aiogram.filters import CommandStart
 from aiogram_dialog import DialogManager, StartMode
 
+from app.crud.user import create_user, user_exits_by_telegram_id
 from app.dialogs.start.states import StartMenu
 from app.handlers.router import router
 
 
 @router.message(CommandStart())
 async def start_handler(message: Message, dialog_manager: DialogManager):
+    if not await user_exits_by_telegram_id(int(message.from_user.id)):
+        await create_user(int(message.from_user.id), message.from_user.username)
     await dialog_manager.start(StartMenu.select_menu, mode=StartMode.RESET_STACK)
+
