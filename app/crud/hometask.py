40c29064cf431e2ee73a3d192811d8aaeeb92c04
fadegor05 <fad.egor05@gmail.com>
2024-04-06 20:06:23 +0400

    added create hometask in crud

--- app/crud/hometask.py
+++ app/crud/hometask.py
@@ -1,7 +1,21 @@
+from uuid import uuid4
+from datetime import datetime
+
 from app.database import mongo_connection, mongo_get_collection
 
 
 async def get_hometasks_all():
     connection = await mongo_connection()
-    user_collection = await mongo_get_collection(connection, 'hometasks')
-    return user_collection.find({})
+    hometask_collection = await mongo_get_collection(connection, 'hometasks')
+    return hometask_collection.find({})
+
+
+async def create_hometask(lesson_uuid: int, task: str, date: datetime):
+    connection = await mongo_connection()
+    hometask_collection = await mongo_get_collection(connection, 'hometasks')
+    hometask_collection.insert_one({
+        'uuid': uuid4(),
+        'lesson_uuid': lesson_uuid,
+        'task': task,
+        'date': date
+    })
