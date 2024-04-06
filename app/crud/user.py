5ee996008f4196678708311b337bf8754fba13df
fadegor05 <fad.egor05@gmail.com>
2024-04-06 23:58:34 +0400

    updated user crud

--- app/crud/user.py
+++ app/crud/user.py
@@ -1,10 +1,19 @@
 from app.database import mongo_get_collection, mongo_connection
 
 
-async def user_exits_by_telegram_id(telegram_id: int) -> bool:
+async def get_user_by_telegram_id(telegram_id: int):
     connection = await mongo_connection()
     user_collection = await mongo_get_collection(connection, 'users')
-    return True if user_collection.find_one({'telegram_id': telegram_id}) else False
+    return user_collection.find_one({'telegram_id': telegram_id})
+
+
+async def is_user_editor_by_telegram_id(telegram_id: int):
+    user = await get_user_by_telegram_id(telegram_id)
+    return True if user.get('is_editor') else False
+
+
+async def user_exits_by_telegram_id(telegram_id: int) -> bool:
+    return True if await get_user_by_telegram_id(telegram_id) else False
 
 
 async def create_user(telegram_id: int, username: str, specialization: str = None):
