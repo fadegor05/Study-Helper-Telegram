82ee5924bad9eba84723eb5f7f2a49f733097cdf
fadegor05 <fad.egor05@gmail.com>
2024-04-06 19:00:06 +0400

    added database get collection util function

--- app/database.py
+++ app/database.py
@@ -1,4 +1,7 @@
+from typing import Any, Mapping
+
 from pymongo import MongoClient
+from pymongo.collection import Collection
 
 from app.core.config import MONGO_URL, MONGO_DATABASE_NAME
 
@@ -18,3 +21,11 @@ async def mongo_init(connection: MongoClient, database_name: str = MONGO_DATABAS
     for collection in COLLECTIONS:
         if collection not in db.list_collection_names():
             db.create_collection(collection)
+
+
+async def mongo_get_collection(connection: MongoClient, collection_name: str,
+                               database_name: str = MONGO_DATABASE_NAME) -> Collection[Mapping[str, Any] | Any] | None:
+    if collection_name not in COLLECTIONS:
+        return None
+    db = connection.get_database(database_name)
+    return db.get_collection(collection_name)
