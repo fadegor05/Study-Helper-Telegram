0ed7829015ab1617fb136bd5b5a0ec776d3d80e5
Egor Fadeev <116910468+fadegor05@users.noreply.github.com>
2024-08-25 17:54:47 +0400

    added hometask notification

--- bot/app/__main__.py
+++ bot/app/__main__.py
@@ -3,12 +3,15 @@ import logging
 import sys
 
 from aiogram import Bot, Dispatcher
+from apscheduler.triggers.cron import CronTrigger
 
+from app.apsched.hometask import hometask_notification
 from app.database import mongo_connection, mongo_init
 from aiogram_dialog import setup_dialogs
 from app.dialogs import get_dialogs
 from app.handlers.router import router
 from app.core.config import TELEGRAM_TOKEN, MONGO_DATABASE_NAME
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
 
 
 async def main() -> None:
@@ -18,6 +21,10 @@ async def main() -> None:
     dp = Dispatcher()
     dp.include_routers(router, *get_dialogs())
     setup_dialogs(dp)
+
+    scheduler = AsyncIOScheduler(timezone="Europe/Moscow")
+    scheduler.add_job(hometask_notification, trigger=CronTrigger(day_of_week='0-4,6',hour=16, minute=0), kwargs={'bot': bot})
+    scheduler.start()
 
     await dp.start_polling(bot)
 
