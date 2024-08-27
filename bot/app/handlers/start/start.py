42dca868dec278185dd0f00552f6290cc3f434cc
Egor Fadeev <116910468+fadegor05@users.noreply.github.com>
2024-08-27 17:43:07 +0400

    added user access control

--- bot/app/handlers/start/start.py
+++ bot/app/handlers/start/start.py
@@ -2,7 +2,7 @@ from aiogram.types import Message
 from aiogram.filters import CommandStart
 from aiogram_dialog import DialogManager, StartMode
 
-from app.crud.user import create_user, user_exits_by_telegram_id
+from app.crud.user import create_user, user_exits_by_telegram_id, have_user_access_by_telegram_id
 from app.dialogs.start.states import StartMenu
 from app.handlers.router import router
 
@@ -11,5 +11,8 @@ from app.handlers.router import router
 async def start_handler(message: Message, dialog_manager: DialogManager):
     if not await user_exits_by_telegram_id(int(message.from_user.id)):
         await create_user(int(message.from_user.id), message.from_user.username)
+    if not await have_user_access_by_telegram_id(int(message.from_user.id)):
+        await message.answer('*Упс...* К сожалению, у вас нет доступа к этом боту ⛔️️')
+        return
     await dialog_manager.start(StartMenu.select_menu, mode=StartMode.RESET_STACK)
 
