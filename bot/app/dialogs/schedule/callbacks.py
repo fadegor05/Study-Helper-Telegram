819b8234f30e2d0217cf003201214a1f75b6e018
Egor Fadeev <116910468+fadegor05@users.noreply.github.com>
2024-08-31 17:21:23 +0400

    fixed null schedule bug

--- bot/app/dialogs/schedule/callbacks.py
+++ bot/app/dialogs/schedule/callbacks.py
@@ -4,6 +4,7 @@ from aiogram.types import CallbackQuery
 from aiogram_dialog import DialogManager
 from aiogram_dialog.widgets.kbd import Select, Button
 
+from app.crud.schedule import get_day_schedule
 from app.dialogs.schedule.states import ScheduleInfo
 
 
@@ -15,11 +16,17 @@ async def on_chosen_today(c: CallbackQuery, widget: Button, manager: DialogManag
     day = datetime.today().isoweekday()
     if day == 7:
         day = 6
-    await manager.start(ScheduleInfo.info_schedule, {'schedule_day': day})
+    if await get_day_schedule(day):
+        await manager.start(ScheduleInfo.info_schedule, {'schedule_day': day})
+    else:
+        await c.answer('Расписание на данный день отсутствует ❌')
 
 
 async def on_chosen_tomorrow(c: CallbackQuery, widget: Button, manager: DialogManager):
     day = datetime.today().isoweekday() + 1
     if day == 8:
         day = 1
-    await manager.start(ScheduleInfo.info_schedule, {'schedule_day': day})
+    if await get_day_schedule(day):
+        await manager.start(ScheduleInfo.info_schedule, {'schedule_day': day})
+    else:
+        await c.answer('Расписание на данный день отсутствует ❌')
