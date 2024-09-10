d4b9810304da5ffcb16a3d4588c99ef536a96dfa
Egor Fadeev <116910468+fadegor05@users.noreply.github.com>
2024-09-10 21:03:36 +0400

    added material base crud

---
+++ bot/app/crud/material.py
@@ -0,0 +1,22 @@
+from uuid import uuid4
+
+from app.database import mongo_connection, mongo_get_collection
+
+
+async def create_material(name: str, link: str, lesson_uuid: str):
+    connection = await mongo_connection()
+    material_collection = await mongo_get_collection(connection, "materials")
+    material_collection.insert_one(
+        {
+            "uuid": str(uuid4()),
+            "name": name,
+            "lesson_uuid": lesson_uuid,
+            "link": link
+        }
+    )
+
+
+async def get_material_by_uuid(uuid: str):
+    connection = await mongo_connection()
+    material_collection = await mongo_get_collection(connection, "materials")
+    return material_collection.find_one({"uuid": uuid})
