d4e498e65b5a556ab7f9ad44b19ac0fa1a359aab
Egor Fadeev <116910468+fadegor05@users.noreply.github.com>
2024-09-12 19:07:14 +0400

    added schedule pagination

--- bot/app/dialogs/schedule/windows.py
+++ bot/app/dialogs/schedule/windows.py
@@ -1,5 +1,5 @@
 from aiogram_dialog import Window
-from aiogram_dialog.widgets.kbd import Cancel, Button
+from aiogram_dialog.widgets.kbd import Cancel, Button, Row
 from aiogram_dialog.widgets.text import Format, Const
 
 from app.dialogs.schedule import states, keyboards, callbacks, getters
@@ -23,6 +23,7 @@ def schedule_day_window():
     return Window(
         Format("*{name} *📆"),
         keyboards.column_lessons(),
+        Row(Button(Const("◀️ Предыдущий день"), "previous_day_button", callbacks.previous_day), Button(Const("Следующий день ▶️"), "next_day_button", callbacks.next_day)),
         Cancel(Const("⬅️ Назад")),
         state=states.ScheduleInfo.info_schedule,
         getter=getters.get_schedule_day,
