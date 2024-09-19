f1d7515345471c8f98fe48e7dd4fbe12545f987f
Egor Fadeev <116910468+fadegor05@users.noreply.github.com>
2024-09-19 18:44:25 +0400

    added show hometask by lesson in schedule

--- bot/app/dialogs/schedule/keyboards.py
+++ bot/app/dialogs/schedule/keyboards.py
@@ -5,12 +5,13 @@ from aiogram_dialog.widgets.text import Format
 
 
 
-def column_lessons():
+def column_lessons(on_click):
     return Column(
         Select(
             Format("{item[start_time]} {item[name]} {item[place]}"),
             id="s_scroll_lessons",
             item_id_getter=operator.itemgetter("lesson_uuid"),
             items="lessons",
+            on_click=on_click,
         )
     )
