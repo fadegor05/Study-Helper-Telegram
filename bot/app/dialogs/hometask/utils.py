17881af401e3a5022e69b53e8a2c57300828085d
Egor Fadeev <116910468+fadegor05@users.noreply.github.com>
2024-10-29 14:21:23 +0400

    added extra utils to homework dialog

--- bot/app/dialogs/hometask/utils.py
+++ bot/app/dialogs/hometask/utils.py
@@ -8,6 +8,10 @@ def is_editor(data: Dict, widget: Whenable, manager: DialogManager):
     return data.get("is_editor")
 
 
+def is_not_editor(data: Dict, widget: Whenable, manager: DialogManager):
+    return not data.get("is_editor")
+
+
 def is_lesson_in_schedule(data: Dict, widget: Whenable, manager: DialogManager):
     return data.get("in_schedule")
 
@@ -18,3 +22,15 @@ def is_lesson_not_in_schedule(data: Dict, widget: Whenable, manager: DialogManag
 
 def is_not_completed(data: Dict, widget: Whenable, manager: DialogManager):
     return not data.get("is_completed")
+
+
+def is_suggested(data: Dict, widget: Whenable, manager: DialogManager):
+    return data.get("is_suggested")
+
+
+def is_not_suggested(data: Dict, widget: Whenable, manager: DialogManager):
+    return not data.get("is_suggested")
+
+
+def is_not_completed_and_not_suggested(data: Dict, widget: Whenable, manager: DialogManager):
+    return not data.get("is_completed") and not data.get("is_suggested")
